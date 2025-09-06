01ab4a7afd22ea027aa0189d22613c0e3703db4a
Reeni Rivera <159098065+CodeWolfHound@users.noreply.github.com>
2025-09-06 12:18:48 -0500

    Add conditional logic learning

--- Main.cpp
+++ Main.cpp
@@ -6,3 +6,17 @@ int main() {
     int result = Multiplay(4, 5);
     log(result);
 }
+
+int x = 5;
+if (x == 6)
+{
+	log("True");
+}
+else if (x == 5)
+{
+	log("Theoretical");
+}
+else
+{
+	log("False");
+}
